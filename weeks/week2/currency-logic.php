cc51af540ed286b90f92557be26da6296e30b4dd
MayHyeyeonKim <hkim7963@gmail.com>
2024-01-13 03:50:26 -0800

    added weeks/week2/currency.php

--- weeks/week2/currency-logic.php
+++ weeks/week2/currency-logic.php
@@ -19,7 +19,7 @@
 }
 
 table {
-    border:1px solid orange;
+    border:1px solid blueviolet;
     border-collapse:collapse;
     width:500px;
 }
@@ -41,48 +41,58 @@ h1, h2, h3  {
 <body>
 <div id="wrapper">
 <h1>After our world-wind travels, we have returned home with the following currencies</h1>
+
 <ul>
 <li>Rubles</li>
 <li>Pound sterling</li>
-<li>Canadian Dollar</li>
+<li>Canadian Dollars</li>
 <li>Euros</li>
 <li>Yens</li>
-
 </ul>
+
 <h2>Whatever shall we do?</h2>
 
 <table>
 <tr>
 <th colspan="2">Currency</th>
 <th>Dollars</th>
-
 </tr>
+
 <tr>
 <th>Rubles</th>
 <td>10007</td>
 <td>117.11</td>
 </tr>
+
 <tr>
 <th>Pound sterling</th>
 <td>500</td>
 <td>575</td>
 </tr>
+
 <tr>
 <th>Canadian Dollars</th>
 <td>5000</td>
 <td>3800</td>
 </tr>
+
 <tr>
 <th>Euros</th>
 <td>1200</td>
 <td>1200</td>
 </tr>
+
 <tr>
 <th>Yens</th>
 <td>2000</td>
 <td>14.80</td>
 </tr>
 
+<!-- <tr>
+<th>Total</th>
+<td></td>
+<td></td>
+</tr> -->
 
 
 </table>
