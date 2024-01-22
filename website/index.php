d62e1f9539401fd8910ab8d184aca8068276519d
MayHyeyeonKim <hkim7963@gmail.com>
2024-01-21 22:50:41 -0800

    website/index.php | includes

---
+++ website/index.php
@@ -0,0 +1,53 @@
+<?php include('./includes/header.php');?>
+  <div id="wrapper">
+
+    <div id="hero">
+      <img src="images/twelve.jpg" alt="twelve">
+    </div>
+    <!-- end hero -->
+
+    <main>
+      <h1>Welcome to my first PHP journey!</h1>
+      <h2>I am going to learn PHP!</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut neque libero, cursus nec nisl ut, pretium
+        posuere orci. Sed laoreet venenatis massa, in vulputate metus posuere sed. Suspendisse condimentum
+        tempus consectetur. Morbi eget ex ipsum. Pellentesque interdum sollicitudin ex, vitae dictum est iaculis
+        id. Ut ut maximus sapien, sit amet vulputate urna. Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Nulla vulputate consequat leo, et cursus ante. </p>
+      <h2>Let's gooooo!!</h2>
+      <p>Sed commodo ante ut libero luctus sagittis. Maecenas consequat, elit ut bibendum dignissim, dui est
+        elementum tellus, a dignissim nibh ante quis nisi. Donec leo neque, dignissim at pretium in, dictum at
+        magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+    </main>
+
+
+    <aside>
+      <h3>Class Exercises</h3>
+      <h3>Week 1:</h3>
+      <ul>
+        <li>
+          <a href="https://github.com/mayhyeyeonkim" title="Link to Github Profile">
+            GitHub Profile
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/mayhyeyeonkim/it261" title="Link to it261 GitHub repository">
+            it261 GitHub repository
+          </a>
+
+        </li>
+      </ul>
+      <h3>Week 2:</h3>
+      <ul>
+        <li><a href="" title="">var.php</a></li>
+        <li><a href="" title="">var2.php</a></li>
+        <li><a href="" title="">currency-logic.php</a></li>
+        <li><a href="" title="">hereDoc.php</a></li>
+      </ul>
+      <h3>Week 3:</h3>
+      <h3>Week 4:</h3>
+
+    </aside>
+  </div>
+  <!-- end wrapper -->
+  <?php include './includes/footer.php';
