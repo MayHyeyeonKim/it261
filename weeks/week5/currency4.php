c45f5ebcfb77872bff979f750c4c2b17e11c7b96
MayHyeyeonKim <hkim7963@gmail.com>
2024-02-05 03:50:15 -0800

    ?

--- weeks/week5/currency4.php
+++ weeks/week5/currency4.php
@@ -180,12 +180,12 @@ if ($dollars >= 10000) {
     <div class="line">
     </div>
 
-    <!-- <footer>
+    <footer>
 <ul>
         <li>Copyright &copy;
-            2022</li>
+            2024</li>
         <li>All Rights Reserved</li>
-        <li><a href="../index.php">Web Design by Edgar</a></li>
+        <li><a href="../index.php">Web Design by May</a></li>
         <li><a id="html-checker" href="#">HTML Validation</a></li>
         <li><a id="css-checker" href="#">CSS Validation</a></li>
         </ul>
@@ -194,7 +194,7 @@ if ($dollars >= 10000) {
                 document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
                 document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);
         </script>  
-</footer> -->
+</footer>
 
 
 
