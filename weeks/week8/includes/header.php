b7feae9252e985d8ccf11ad6294f8764fba61cb4
MayHyeyeonKim <hkim7963@gmail.com>
2024-02-25 02:16:22 -0800

    ahhhhhhh~~ added gitignore

--- weeks/week8/includes/header.php
+++ weeks/week8/includes/header.php
@@ -1,15 +1,11 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Thank You Page!</title>
+    <title>May's Week8 People Exercise!</title>
     <link href="css/styles.css" type="text/css" rel="stylesheet">
 </head>
 <body>
-<h1>Thank you for filling out our form!</h1>
-<h1>from May</h1>
-</body>
-</html>
+    
