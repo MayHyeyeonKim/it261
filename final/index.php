d14132788ab899ee649cdebe55d6345d9fb877ee
MayHyeyeonKim <hkim7963@gmail.com>
2024-03-17 23:10:07 -0700

    final project

---
+++ final/index.php
@@ -0,0 +1,8 @@
+<?php
+session_start(); // Starts a new session or resumes an existing session to maintain user state across page requests.
+
+include('config.php');
+include('./includes/header.php');
+?>
+
+<?php include('./includes/footer.php');?>
